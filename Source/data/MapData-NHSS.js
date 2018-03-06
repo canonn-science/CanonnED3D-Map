6b5b2d63cc4649fa7505b3f3c3d8f34a1f2ce88c
DMehaffy <derrickmehaffy@gmail.com>
2018-03-06 04:21:22 -0700

    Change NHSS Source and add Threat Levels

--- Source/data/MapData-NHSS.js
+++ Source/data/MapData-NHSS.js
@@ -9,12 +9,40 @@ var canonnEd3d_nhss = {
 					"color": "FF9D00"
 				}
 			},
-			"Non-Human Signal Source - (NHSS)": {
-				"1400": {
-					"name": "Non-Human Signal Source",
-					"color": "442299"
-				}
-			}
+			"Non-Human Signal Source - (NHSS)":  {
+                "1400": {
+                    "name": "Threat Level 2",
+                    "color": "99b433"
+                },
+                "1401": {
+                    "name": "Threat Level 3",
+                    "color": "1e7145"
+                },
+                "1402": {
+                    "name": "Threat Level 4",
+                    "color": "ff0097"
+                },
+                "1403": {
+                    "name": "Threat Level 5",
+                    "color": "b91d47"
+                },
+                "1404": {
+                    "name": "Threat Level 6",
+                    "color": "e3a21a"
+                },
+                "1405": {
+                    "name": "Threat Level 7",
+                    "color": "603cba"
+                },
+                "1406": {
+                    "name": "Threat Level 8",
+                    "color": "da532c"
+                },
+                "1407": {
+                    "name": "Threat Level Unknown",
+                    "color": "8B0000"
+                }
+            }
 		},
 		"systems": [{
 			"name": "Sol",
@@ -50,28 +78,46 @@ var canonnEd3d_nhss = {
 	},
 
 	formatNHSS: function (data) {
+        //Here you format BN JSON to ED3D acceptable object
 
-		//Here you format NHSS JSON to ED3D acceptable object
+        // this is assuming data is an array []
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].System && data[i].System.replace(" ", "").length > 1) {
+                var nhssSite = {};
+                nhssSite["name"] = data[i].System;
 
-		// this is assuming data is an array []
-		for (var i = 0; i < data.length; i++) {
-			if (data[i].system && data[i].system.replace(" ", "").length > 1) {
-				var nhssSite = {};
-				nhssSite["name"] = data[i].system;
-				nhssSite["cat"] = [1400];
-				nhssSite["coords"] = {
-					"x": parseFloat(data[i].galacticX),
-					"y": parseFloat(data[i].galacticY),
-					"z": parseFloat(data[i].galacticZ)
-				};
+				// Check Threat Level and apply group
+				if (data[i].Threat.toString() == "1") {
+					grSite["cat"] = [1400];
+				} else if (data[i].type.toString() == "2") {
+					grSite["cat"] = [1401];
+				} else if (data[i].type.toString() == "3") {
+					grSite["cat"] = [1402];
+				} else if (data[i].type.toString() == "4") {
+					grSite["cat"] = [1403];
+				} else if (data[i].type.toString() == "5") {
+					grSite["cat"] = [1404];
+				} else if (data[i].type.toString() == "6") {
+					grSite["cat"] = [1405];
+				} else if (data[i].type.toString() == "7") {
+					grSite["cat"] = [1406];
+				} else if (data[i].type.toString() == "8") {
+					grSite["cat"] = [1407];
+				} else {
+					grSite["cat"] = [1408];
+				}
 
-				// We can then push the site to the object that stores all systems
-				canonnEd3d_nhss.systemsData.systems.push(nhssSite);
-			}
+                nhssSite["coords"] = {
+                    "x": parseFloat(data[i].x),
+                    "y": parseFloat(data[i].y),
+                    "z": parseFloat(data[i].z)
+                };
 
-		}
-
-	},
+                // We can then push the site to the object that stores all systems
+                canonnEd3d_nhss.systemsData.systems.push(nhssSite);
+            }
+        }
+    },
 
 	parseData: function (url, callBack, resolvePromise) {
 		Papa.parse(url, {
@@ -94,7 +140,7 @@ var canonnEd3d_nhss = {
 
 		//NHSS Sites
 		var p1 = new Promise(function (resolve, reject) {
-			canonnEd3d_nhss.parseData("data/csvCache/nhssDataCache.csv", canonnEd3d_nhss.formatNHSS, resolve);
+			canonnEd3d_nhss.parseData("https://docs.google.com/spreadsheets/d/e/2PACX-1vROqL6zifWWxcwlZ0R6iLvrMrUdfJijnMoZee-SrN0NVPqhTdH3Zdx6E7RxP1wH2xgwfrhwfVWUHnKU/pub?gid=1590459372&single=true&output=csv", canonnEd3d_nhss.formatNHSS, resolve);
 		});
 
 		Promise.all([p1]).then(function () {
