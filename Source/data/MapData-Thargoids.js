a72bfae8950ad377d2b0d8496035918cde17cd8d
NoFoolLikeOne <github@paparomeo.uk>
2018-03-17 16:52:17 +0000

    Show only recent history
    This allows two modes. show all NHSS ever or show the most recent 100 NHSS

--- Source/data/MapData-Thargoids.js
+++ Source/data/MapData-Thargoids.js
@@ -440,9 +440,24 @@ var canonnEd3d_thargoids = {
         });
 
         // Thargoid US
+		// We can default to everything or havea parameter to only show the last 100 systems
+		 try {
+            var history = $.urlParam('history');
+        } catch (err) {
+            console.log(err)
+            var history = "all";
+        }
+		
+		if ( history == "recent" ) {
         var p5 = new Promise(function (resolve, reject) {
-            canonnEd3d_thargoids.parseData("https://docs.google.com/spreadsheets/d/e/2PACX-1vROqL6zifWWxcwlZ0R6iLvrMrUdfJijnMoZee-SrN0NVPqhTdH3Zdx6E7RxP1wH2xgwfrhwfVWUHnKU/pub?gid=1590459372&single=true&output=csv", canonnEd3d_thargoids.formatTI, resolve);
+			canonnEd3d_thargoids.parseData("https://docs.google.com/spreadsheets/d/e/2PACX-1vROqL6zifWWxcwlZ0R6iLvrMrUdfJijnMoZee-SrN0NVPqhTdH3Zdx6E7RxP1wH2xgwfrhwfVWUHnKU/pub?gid=1692836450&single=true&output=csv", canonnEd3d_thargoids.formatTI, resolve);
         });
+		} else {
+		var p5 = new Promise(function (resolve, reject) {
+			canonnEd3d_thargoids.parseData("https://docs.google.com/spreadsheets/d/e/2PACX-1vROqL6zifWWxcwlZ0R6iLvrMrUdfJijnMoZee-SrN0NVPqhTdH3Zdx6E7RxP1wH2xgwfrhwfVWUHnKU/pub?gid=1590459372&single=true&output=csv", canonnEd3d_thargoids.formatTI, resolve);
+        });	
+		}
+	
 
         // Thargoid Hyperdictions
         var p6 = new Promise(function (resolve, reject) {
