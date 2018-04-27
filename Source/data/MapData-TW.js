3f137642eecaba52d64f28c80e7e7c8f93db895c
DMehaffy <derrickmehaffy@gmail.com>
2018-04-27 06:23:56 -0700

    Added Beta Tube Worms Map
    Data is pulled from EXTool data, not cached server side.

---
+++ Source/data/MapData-TW.js
@@ -0,0 +1,115 @@
+var canonnEd3d_tw = {
+
+	//Define Categories
+	systemsData: {
+		"categories": {
+			"POI Systems": {
+				"100": {
+					"name": "Systems - POI",
+					"color": "FF9D00"
+				}
+			},
+			"Tube Worms - (TW)": {
+				"500": {
+					"name": "Tube Worm",
+					"color": "ffc266"
+				}
+			}
+		},
+		"systems": [{
+			"name": "Sol",
+			"coords": {
+				"x": "0",
+				"y": "0",
+				"z": "0"
+			},
+			"cat": [
+				"100"
+			]
+		}, {
+			"name": "Merope",
+			"coords": {
+				"x": "-78.59375",
+				"y": "-149.625",
+				"z": "-340.53125"
+			},
+			"cat": [
+				"100"
+			]
+		}, {
+			"name": "HIP 22460",
+			"coords": {
+				"x": "-41.3125",
+				"y": "-58.96875",
+				"z": "-354.78125"
+			},
+			"cat": [
+				"100"
+			]
+		}]
+	},
+
+	formatTW: function (data) {
+
+		//Here you format TW JSON to ED3D acceptable object
+
+		// this is assuming data is an array []
+		for (var i = 0; i < data.length; i++) {
+			if (data[i].system && data[i].system.replace(" ", "").length > 1) {
+				var twSite = {};
+				twSite["name"] = data[i].system;
+				twSite["cat"] = [500];
+				twSite["coords"] = {
+					"x": parseFloat(data[i].galacticX),
+					"y": parseFloat(data[i].galacticY),
+					"z": parseFloat(data[i].galacticZ)
+				};
+
+				// We can then push the site to the object that stores all systems
+				canonnEd3d_tw.systemsData.systems.push(twSite);
+			}
+
+		}
+
+	},
+
+	parseData: function (url, callBack, resolvePromise) {
+		Papa.parse(url, {
+			download: true,
+			header: true,
+			complete: function (results) {
+
+				callBack(results.data);
+
+				// after we called the callback
+				// (which is synchronous, so we know it's safe here)
+				// we can resolve the promise
+
+				resolvePromise();
+			}
+		});
+	},
+
+	init: function () {
+
+		//FM Sites
+		var p1 = new Promise(function (resolve, reject) {
+			canonnEd3d_tw.parseData("https://docs.google.com/spreadsheets/d/e/2PACX-1vTgopadbnAXaN2hOSut9bHTW7KixDHR3qzt1RtMrqPlUodimCg1HMs9WmdQ35ZXvLeSpu2azvC__IzB/pub?gid=926585382&single=true&output=csv", canonnEd3d_tw.formatTW, resolve);
+		});
+
+		Promise.all([p1]).then(function () {
+			Ed3d.init({
+				container: 'edmap',
+				json: canonnEd3d_tw.systemsData,
+				withFullscreenToggle: false,
+				withHudPanel: true,
+				hudMultipleSelect: true,
+				effectScaleSystem: [50, 10000],
+				startAnim: false,
+				showGalaxyInfos: true,
+				cameraPos: [25, 14100, -12900],
+				systemColor: '#FF9D00'
+			});
+		});
+	}
+};
