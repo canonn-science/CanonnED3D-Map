e715668cfb0bb13aac0828607ae3f5c4d751ce61
DMehaffy <derrickmehaffy@gmail.com>
2018-08-22 03:35:01 -0700

    Add OI Map

--- Source/data/MapData-All.js
+++ Source/data/MapData-All.js
@@ -56,6 +56,10 @@ var canonnEd3d_all = {
 					"name": "Lave Spouts (LS)",
 					"color": "#ff4d4d"
 				},
+				"1001": {
+					"name": "Orbital Installations (OI)",
+					"color": "#ff764d"
+				},
 				"1200": {
 					"name": "Thargoid Barnacles (TB)",
 					"color": "#009933"
@@ -412,6 +416,30 @@ var canonnEd3d_all = {
 
 	},
 
+	formatOI: function (data) {
+
+		//Here you format OI JSON to ED3D acceptable object
+
+		// this is assuming data is an array []
+		for (var i = 0; i < data.length; i++) {
+			if (data[i].system && data[i].system.replace(" ", "").length > 1) {
+				var oiSite = {};
+				oiSite["name"] = data[i].system;
+				oiSite["cat"] = [1001];
+				oiSite["coords"] = {
+					"x": parseFloat(data[i].galacticX),
+					"y": parseFloat(data[i].galacticY),
+					"z": parseFloat(data[i].galacticZ)
+				};
+
+				// We can then push the site to the object that stores all systems
+				canonnEd3d_all.systemsData.systems.push(oiSite);
+			}
+
+		}
+
+	},
+
 	formatMS: function (data) {
 
 		//Format MS JSON to ED3D acceptable object
@@ -577,27 +605,32 @@ var canonnEd3d_all = {
 			canonnEd3d_all.parseData("data/csvCache/lsSystemCache.csv", canonnEd3d_all.formatLS, resolve);
 		});
 
-		//MS Sites
+		//OI Sites
 		var p11 = new Promise(function (resolve, reject) {
+			canonnEd3d_all.parseData("data/csvCache/oiSystemCache.csv", canonnEd3d_all.formatOI, resolve);
+		});
+
+		//MS Sites
+		var p12 = new Promise(function (resolve, reject) {
 			canonnEd3d_all.parseData("data/csvCache/msSystemCache.csv", canonnEd3d_all.formatMS, resolve);
 		});
 
 		//TB Sites
-		var p12 = new Promise(function (resolve, reject) {
+		var p13 = new Promise(function (resolve, reject) {
 			canonnEd3d_all.parseData("data/csvCache/tbSystemCache.csv", canonnEd3d_all.formatTB, resolve);
 		});
 
 		//TS Sites
-		var p13 = new Promise(function (resolve, reject) {
+		var p14 = new Promise(function (resolve, reject) {
 			canonnEd3d_all.parseData("data/csvCache/tsSystemCache.csv", canonnEd3d_all.formatTS, resolve);
 		});
 
 		//NHSS Sites
-		var p14 = new Promise(function (resolve, reject) {
+		var p15 = new Promise(function (resolve, reject) {
 			canonnEd3d_all.parseData("data/csvCache/nhssDataCache.csv", canonnEd3d_all.formatNHSS, resolve);
 		});
 
-		Promise.all([p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14]).then(function () {
+		Promise.all([p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15]).then(function () {
 			Ed3d.init({
 				container: 'edmap',
 				json: canonnEd3d_all.systemsData,
---
+++ Source/data/MapData-OI.js
@@ -0,0 +1,115 @@
+var canonnEd3d_oi = {
+
+	//Define Categories
+	systemsData: {
+		"categories": {
+			"POI Systems": {
+				"100": {
+					"name": "Systems - POI",
+					"color": "FF9D00"
+				}
+			},
+			"Orbital Installations - (OI)": {
+				"1000": {
+					"name": "Orbital Installation",
+					"color": "ff4d4d"
+				}
+			}
+		},
+		"systems": [{
+			"name": "Sol",
+			"coords": {
+				"x": "0",
+				"y": "0",
+				"z": "0"
+			},
+			"cat": [
+				"100"
+			]
+		}, {
+			"name": "Merope",
+			"coords": {
+				"x": "-78.59375",
+				"y": "-149.625",
+				"z": "-340.53125"
+			},
+			"cat": [
+				"100"
+			]
+		}, {
+			"name": "HIP 22460",
+			"coords": {
+				"x": "-41.3125",
+				"y": "-58.96875",
+				"z": "-354.78125"
+			},
+			"cat": [
+				"100"
+			]
+		}]
+	},
+
+	formatOI: function (data) {
+
+		//Here you format OI JSON to ED3D acceptable object
+
+		// this is assuming data is an array []
+		for (var i = 0; i < data.length; i++) {
+			if (data[i].system && data[i].system.replace(" ", "").length > 1) {
+				var oiSite = {};
+				oiSite["name"] = data[i].system;
+				oiSite["cat"] = [1000];
+				oiSite["coords"] = {
+					"x": parseFloat(data[i].galacticX),
+					"y": parseFloat(data[i].galacticY),
+					"z": parseFloat(data[i].galacticZ)
+				};
+
+				// We can then push the site to the object that stores all systems
+				canonnEd3d_oi.systemsData.systems.push(oiSite);
+			}
+
+		}
+
+	},
+
+	parseData: function (url, callBack, resolvePromise) {
+		Papa.parse(url, {
+			download: true,
+			header: true,
+			complete: function (results) {
+
+				callBack(results.data);
+
+				// after we called the callback
+				// (which is synchronous, so we know it's safe here)
+				// we can resolve the promise
+
+				resolvePromise();
+			}
+		});
+	},
+
+	init: function () {
+
+		//OI Sites
+		var p1 = new Promise(function (resolve, reject) {
+			canonnEd3d_oi.parseData("data/csvCache/oiDataCache.csv", canonnEd3d_oi.formatOI, resolve);
+		});
+
+		Promise.all([p1]).then(function () {
+			Ed3d.init({
+				container: 'edmap',
+				json: canonnEd3d_oi.systemsData,
+				withFullscreenToggle: false,
+				withHudPanel: true,
+				hudMultipleSelect: true,
+				effectScaleSystem: [50, 10000],
+				startAnim: false,
+				showGalaxyInfos: true,
+				cameraPos: [25, 14100, -12900],
+				systemColor: '#FF9D00'
+			});
+		});
+	}
+};
