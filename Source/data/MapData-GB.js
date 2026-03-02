6bd4efbe07ed0a4c5e263e9fefa94a25135cd27d
NoFoolLikeOne <31373535+NoFoolLikeOne@users.noreply.github.com>
2026-03-02 00:56:24 +0000

    Restore the Guardians

--- Source/data/MapData-GB.js
+++ Source/data/MapData-GB.js
@@ -1,51 +1,3 @@
-const API_ENDPOINT = `https://api.canonn.tech`;
-const API_LIMIT = 1000;
-
-const capi = axios.create({
-	baseURL: API_ENDPOINT,
-	headers: {
-		'Content-Type': 'application/json',
-		'Accept': 'application/json',
-	},
-});
-
-let sites = {
-	gbsites: [],
-};
-
-const streamSitesForType = async (type, formatCallback) => {
-	let API_START = 0;
-	let keepGoing = true;
-	while (keepGoing) {
-		let response = await reqSites(API_START, type);
-		// Call the format callback with this batch and the type
-		await formatCallback(response.data, type);
-		API_START += API_LIMIT;
-		if (response.data.length < API_LIMIT) {
-			keepGoing = false;
-		}
-		// Yield to event loop after each batch
-		await new Promise(resolve => setTimeout(resolve, 0));
-	}
-};
-
-const streamAllSites = async (callback) => {
-	const types = Object.keys(sites);
-	for (const type of types) {
-		await streamSitesForType(type, callback);
-	}
-};
-
-const reqSites = async (API_START, type) => {
-
-	let payload = await capi({
-		url: `/${type}?_limit=${API_LIMIT}&_start=${API_START}`,
-		method: 'get'
-	});
-
-	return payload;
-};
-
 var canonnEd3d_gb = {
 
 	//Define Categories
@@ -56,57 +8,53 @@ var canonnEd3d_gb = {
 					name: "Beacon",
 					color: randomColor().replace('#', '').toString()
 				}
-			},
-			'Unknown Type': {
-				'2000': {
-					name: 'Unknown Site',
-					color: '800000',
-				}
 			}
 		},
 		systems: []
 	},
 
-	formatSitesStream: async function (siteDataBatch, siteType) {
-		let formattedBatch = [];
-		for (var d = 0; d < siteDataBatch.length; d++) {
-			if (siteDataBatch[d].system.systemName && siteDataBatch[d].system.systemName.replace(' ', '').length > 1) {
-				var poiSite = {};
-				poiSite['name'] = siteDataBatch[d].system.systemName;
-
-				//Check Site Type and match categories
-				if (siteType == 'gbsites') {
-					poiSite['cat'] = [201];
-				} else {
-					poiSite['cat'] = [2000];
-				}
-				poiSite['coords'] = {
-					x: parseFloat(siteDataBatch[d].system.edsmCoordX),
-					y: parseFloat(siteDataBatch[d].system.edsmCoordY),
-					z: parseFloat(siteDataBatch[d].system.edsmCoordZ),
-				};
-
-				formattedBatch.push(poiSite);
-			}
+	formatBeacons: function (data) {
+		for (let i = 0; i < data.length; i++) {
+			let s = data[i];
+			let name = s["System Name"];
+			if (!name || !name.trim()) continue;
+			let x = parseFloat(String(s["x"]).replace(',', ''));
+			let y = parseFloat(s["y"]);
+			let z = parseFloat(s["z"]);
+			if (isNaN(x) || isNaN(y) || isNaN(z)) continue;
+			let info = 'Guardian Beacon';
+			if (s["Body Name"]) info += '<br>' + s["Body Name"];
+			if (s["Body Sub Type"]) info += ' (' + s["Body Sub Type"] + ')';
+			if (s["Distance To Arrival"]) info += '<br>' + s["Distance To Arrival"] + ' ls';
+			if (s["Guardian Structure System"]) info += '<br>Structure: ' + s["Guardian Structure System"];
+			canonnEd3d_gb.systemsData.systems.push({
+				name: name,
+				coords: { x: x, y: y, z: z },
+				infos: info + '<br>',
+				cat: [201],
+			});
 		}
-		Ed3d.addBatch({systems: formattedBatch});
 	},
 
 	init: function () {
-		Ed3d.init({
-			container: 'edmap',
-			json: canonnEd3d_gb.systemsData,
-			withFullscreenToggle: false,
-			withHudPanel: true,
-			hudMultipleSelect: true,
-			effectScaleSystem: [20, 500],
-			startAnim: true,
-			showGalaxyInfos: true,
-			playerPos: [682, -102, -104],
-			cameraPos: [682 - 300, -102, -104 - 100],
-			systemColor: '#FF9D00'
-		});
-		document.getElementById("loading").style.display = "none";
-		streamAllSites(canonnEd3d_gb.formatSitesStream.bind(canonnEd3d_gb));
-	}
+		fetch('https://storage.googleapis.com/canonn-downloads/guardian_beacons.json')
+			.then(function (r) { return r.json(); })
+			.then(function (data) {
+				canonnEd3d_gb.formatBeacons(data);
+				document.getElementById("loading").style.display = "none";
+				Ed3d.init({
+					container: 'edmap',
+					json: canonnEd3d_gb.systemsData,
+					withFullscreenToggle: false,
+					withHudPanel: true,
+					hudMultipleSelect: true,
+					effectScaleSystem: [20, 500],
+					startAnim: false,
+					showGalaxyInfos: true,
+					playerPos: [682, -102, -104],
+					cameraPos: [382, -102, -204],
+					systemColor: '#FF9D00',
+				});
+			});
+	},
 };
